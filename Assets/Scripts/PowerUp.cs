f1cf8be2d5520c71d27f2b7e1b34564b6448a3e9
Kamil Osuch <kamil.osuch@gmail.com>
2019-06-01 15:15:59 +0200

    Fix missing weapon power up sprite + make it rotate

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -6,6 +6,13 @@ public class PowerUp : MonoBehaviour
 {
     public Weapon powerUpWeapon;
     // Start is called before the first frame update
+
+
+    private void Update()
+    {
+        transform.Rotate(0f, 0f, Time.fixedDeltaTime * 50f, Space.Self);
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         var playerShooter = other.GetComponent<PlayerShooter>();
